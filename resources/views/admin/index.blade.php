82471407663adab4026f8e7e637bc94bca903a27
Coder Lakum <lakummahendra654@gmail.com>
2025-04-26 10:49:09 +0530

    small changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -39,7 +39,7 @@
                     <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-            <div class="col-lg-3 col-xs-6">
+            {{-- <div class="col-lg-3 col-xs-6">
                 <div class="small-box">
                     <div class="inner">
                         <h3>44</h3>
@@ -64,7 +64,7 @@
                     </div>
                     <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </section>
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="footersec">
           <div class="row">
             <div class="col-md-3 col-sm-4 col-xs-12">
-              <p>&copy; {{date('Y')}} Tender World<a href="#"> English</a></p>
+              <p>&copy; {{date('Y')}} {{config('app.name')}}<a href="#"> English</a></p>
             </div>
           </div>
         </div>
