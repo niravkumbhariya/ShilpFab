a4a35cf094a930441d731ae0f1e1e482a01cb2d9
Coder Lakum <lakummahendra654@gmail.com>
2025-07-05 18:41:30 +0530

    keep active

--- resources/views/front/layouts/navbar.blade.php
+++ resources/views/front/layouts/navbar.blade.php
@@ -8,12 +8,16 @@
     </div>
     <nav class="nav-section">
         <ul>
-            <li><a href="{{route('front')}}" class="nav-link active">Home</a></li>
-            <li><a href="{{route('front.work')}}" class="nav-link">Our Work</a></li>
-            <li><a href="{{route('front.about')}}" class="nav-link">About Us</a></li>
-            <li><a href="{{route('front.contact')}}" class="nav-link">Contact Us</a></li>
-            <!-- <li><a href="#" class="btn-primary">Get In Touch</a></li> -->
+            <li><a href="{{ route('front') }}" class="nav-link {{ request()->routeIs('front') ? 'active' : '' }}">Home</a>
+            </li>
+            <li><a href="{{ route('front.work') }}"
+                    class="nav-link {{ request()->routeIs('front.work') ? 'active' : '' }}">Our Work</a></li>
+            <li><a href="{{ route('front.about') }}"
+                    class="nav-link {{ request()->routeIs('front.about') ? 'active' : '' }}">About Us</a></li>
+            <li><a href="{{ route('front.contact') }}"
+                    class="nav-link {{ request()->routeIs('front.contact') ? 'active' : '' }}">Contact Us</a></li>
         </ul>
     </nav>
+
     <div class="hamburger-menu"></div>
 </header>
